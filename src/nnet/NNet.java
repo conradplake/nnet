629f89cbb6f9748f1476fc1d2e58c01f3365ed8f
Conrad Plake <conrad.plake@gmail.com>
2019-05-05 23:00:05 +0200

    Update NNet.java

--- src/nnet/NNet.java
+++ src/nnet/NNet.java
@@ -14,7 +14,7 @@ import java.util.Map;
  * 
  * To speed things up a little, for each neuron, its input and output neurons are cached.
  * 
- * Please be careful with the number of neurons vs. quadratic space consumtion! 
+ * Please be careful with the number of neurons vs. quadratic space consumption! 
  * This implementation is better suited for small networks.
  */
 public class NNet implements NN {
